76f6d010e8b69eab72067215471805d7c554b206
Davyd <davidkrivko@gmail.com>
2023-11-29 23:53:14 +0100

    Add password env

--- login/api.py
+++ login/api.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from config import USERNAME, PASSWORD
 from login.emails import get_verification_code
 
 
