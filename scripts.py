692fedb1d30093d4f2ae0cb82ae3f6d780219f3d
davidkrivko <davidkrivko@gmail.com>
2024-02-15 15:58:28 +0100

    Change logic

--- scripts.py
+++ scripts.py
@@ -20,9 +20,12 @@ async def update_all_bets():
     while True:
         start = datetime.datetime.now()
 
+        if i == 60:
+            await v2_script("line", AUTH_TOKEN)
+            i = 0
+
         tasks = [
             v1_script(),
-            v2_script("line", AUTH_TOKEN),
             v2_script("live", AUTH_TOKEN),
         ]
         await asyncio.gather(*tasks)
@@ -30,3 +33,5 @@ async def update_all_bets():
         i += 1
         stop = datetime.datetime.now()
         logging.error(stop - start)
+
+        await asyncio.sleep(2)
